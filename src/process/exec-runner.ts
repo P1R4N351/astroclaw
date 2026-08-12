98bd12539827c9aa7706dfdff4eab4a1cff0d12b
astroclaw-worker <worker@astroclaw.local>
2026-08-12 13:27:59 -0400

    p10: rewrite src/process/exec-runner.ts
    upstream-sha: 6e880b510776784787ec7884e0c01dc10c2cd103
    run-id: 187250
    worker: astroclaw-distribute
    wing: branch-prime
    nasa-rc: 0

--- src/process/exec-runner.ts
+++ src/process/exec-runner.ts
@@ -1,11 +1,11 @@
 import process from "node:process";
 import { expectDefined } from "@astroclaw/normalization-core";
 import { toErrorObject } from "@astroclaw/normalization-core/error-coercion";
+import { resolveTimerTimeoutMs } from "@astroclaw/normalization-core/number-coercion";
 import {
   decodeWindowsOutputBuffer,
   resolveWindowsConsoleEncoding,
 } from "../infra/windows-encoding.js";
-import { resolveTimerTimeoutMs } from "../shared/number-coercion.js";
 import { releaseChildProcessOutputAfterExit } from "./child-process.js";
 import {
   appendCapturedOutput,
