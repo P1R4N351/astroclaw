6e387d2b27f26767079eb8ee090aedaac76e01f0
astroclaw-worker <worker@astroclaw.local>
2026-08-12 13:19:27 -0400

    p10: rewrite ui/src/pages/usage/helpers.ts
    upstream-sha: bea0e398fa0722ac97732e6fb86e7b196ecfd624
    run-id: 187219
    worker: astroclaw-distribute
    wing: trueblue
    nasa-rc: 0

--- ui/src/pages/usage/helpers.ts
+++ ui/src/pages/usage/helpers.ts
@@ -1,5 +1,5 @@
 // Control UI module implements usage helpers behavior.
-import { normalizeLowercaseStringOrEmpty } from "../../lib/string-coerce.ts";
+import { normalizeLowercaseStringOrEmpty } from "@astroclaw/normalization-core/string-coerce";
 
 type UsageQueryTerm = {
   key?: string;
