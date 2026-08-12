61e778a985475d867371bb25d9cfa7e04aabb755
astroclaw-worker <worker@astroclaw.local>
2026-08-12 13:45:09 -0400

    p10: preprocess src/infra/ports-inspect.ts
    upstream-sha: e689231e42866ba58e8f00f8bb1a69c111372dbe
    run-id: 187338
    worker: astroclaw-worker
    short-circuit: p10-clean-after-preprocess

--- src/infra/ports-inspect.ts
+++ src/infra/ports-inspect.ts
@@ -2,10 +2,10 @@
 import net from "node:net";
 import os from "node:os";
 import { expectDefined } from "@astroclaw/normalization-core";
+import { parseStrictPositiveInteger } from "@astroclaw/normalization-core/number-coercion";
 import { normalizeLowercaseStringOrEmpty } from "@astroclaw/normalization-core/string-coerce";
 import pMap from "p-map";
 import { runCommandWithTimeout } from "../process/exec.js";
-import { parseStrictPositiveInteger } from "./parse-finite-number.js";
 import { buildPortHints } from "./ports-format.js";
 import {
   parseLsofListenerRecordsByPort,
